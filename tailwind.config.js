3d8944bb2d5d0cb675abc5e9802cf0f1d14e3683
ign7 <quaresmaigor@yahoo.com>
2024-01-07 18:41:26 -0300

    gray clor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+const colors = require('tailwindcss/colors') 
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,10 +10,16 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-    ],
+        './vendor/wireui/wireui/tailwind.config.js',
+        './vendor/power-components/livewire-powergrid/tailwind.config.js',
 
+
+    ],
     theme: {
         extend: {
+            colors: {
+                "pg-primary": colors.gray, 
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
