df8e73ec56055644697b27199377ea1651a78a4d
ign7 <quaresmaigor@yahoo.com>
2024-02-13 18:00:15 -0300

    new query custos table

--- app/Livewire/CustosTable.php
+++ app/Livewire/CustosTable.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use App\Models\Custo;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use PowerComponents\LivewirePowerGrid\Column;
 use PowerComponents\LivewirePowerGrid\Footer;
 use PowerComponents\LivewirePowerGrid\Header;
@@ -22,7 +23,30 @@ final class CustosTable extends PowerGridComponent
 
     public function datasource(): ?Collection
     {
-        return Custo::all();
+        DB::statement('SET sql_mode=(SELECT REPLACE(@@sql_mode,"ONLY_FULL_GROUP_BY",""))');
+        return Custo::select([
+            'custos.id as id_custo',
+            'custos.descricao as descricao',
+            'custos.titulo as titulo',
+            'custos.valor_litro as valor_litro',
+            'custos.data_manutencao as data_manutencao',
+            'custos.litros as litros',
+            'custos.combustivel as combustivel',
+            'custos.kimometros as kimometros',
+            'custos.pedagio as pedagio',
+
+            'veiculos.name as name_veiculo',
+            'cargas.numero_carga as numero_carga',
+            'cargas.id as id_carga',
+        ])
+            //->selectRaw('SUM(pedidos.valor_frete) as valor_total_frete_carga')
+            ->leftJoin('cargas', function ($table) {
+                $table->on('custos.carga_id', '=', 'cargas.id');
+            })->join('veiculos', function ($table) {
+                $table->on('custos.veiculo_id', '=', 'veiculos.id');
+            })
+            ->groupBy('custos.id')
+            ->get();
     }
 
     public function setUp(): array
@@ -46,14 +70,14 @@ final class CustosTable extends PowerGridComponent
             ->add('id')
             ->add('titulo')
             ->add('valor_litro')
-            ->add('litro')
+            ->add('litros')
             ->add('combustivel')
             ->add('pedagio')
             ->add('despesas')
             ->add('descarga')
             ->add('manutencao')
-            ->add('carga_id')
-            ->add('veiculo_id')
+            ->add('name_veiculo')
+            ->add('numero_carga')
             ->add('daata_manutencao', function ($entry) {
                 return Carbon::parse($entry->created_at)->format('d/m/Y');
             });
@@ -62,7 +86,7 @@ final class CustosTable extends PowerGridComponent
     public function columns(): array
     {
         return [
-            Column::make('ID', 'id')
+            Column::make('ID', 'id_custo')
                 ->searchable()
                 ->sortable(),
 
@@ -77,7 +101,7 @@ final class CustosTable extends PowerGridComponent
             Column::make('valor_litro', 'valor_litro')
                 ->sortable(),
 
-            Column::make('litro', 'litro')
+            Column::make('litros', 'litros')
                 ->sortable(),
             Column::make('combustivel', 'combustivel')
                 ->sortable(),
@@ -85,9 +109,9 @@ final class CustosTable extends PowerGridComponent
                 ->sortable(),
             Column::make('despesas', 'despesas')
                 ->sortable(),
-            Column::make('manutencao', 'manutencao')
+            Column::make('name_veiculo', 'name_veiculo')
                 ->sortable(),
-            Column::make('carga_id', 'carga_id')
+            Column::make('numero_carga', 'numero_carga')
                 ->sortable(),
 
             Column::make('veiculo_id', 'veiculo_id')
