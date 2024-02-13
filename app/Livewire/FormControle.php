f9c964cd9b600c46701d38135dd26552c7158f9f
ign7 <quaresmaigor@yahoo.com>
2024-02-13 19:35:19 -0300

    ativando acordeon com livewire

--- app/Livewire/FormControle.php
+++ app/Livewire/FormControle.php
@@ -6,6 +6,32 @@ use Livewire\Component;
 
 class FormControle extends Component
 {
+    public $flagcargas, $flagpedidos, $flagclientes;
+
+    public function activecarga()
+    {
+        if (!$this->flagcargas)
+            return $this->flagcargas = true;
+
+        return $this->flagcargas = false;
+    }
+
+    public function activepedido()
+    {
+        if (!$this->flagpedidos)
+            $this->flagpedidos = true;
+
+        return $this->flagpedidos = false;
+    }
+
+
+    public function activecliente()
+    {
+        if (!$this->flagclientes)
+            return  $this->flagclientes = true;
+
+        return $this->flagclientes = false;
+    }
     public function render()
     {
         return view('livewire.form-controle');
--- resources/views/livewire/form-controle.blade.php
+++ resources/views/livewire/form-controle.blade.php
@@ -1,10 +1,7 @@
-
 <div class="py-12">
     <div class="py-4 px-12 pb-8 border-b-2">
-        <h1
-            class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl ">
-            Controle <span
-                class="underline underline-offset-3 decoration-8 decoration-blue-400 decoration-blue-600">de
+        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl ">
+            Controle <span class="underline underline-offset-3 decoration-8 decoration-blue-400 decoration-blue-600">de
                 Custos e Gastos !</span></h1>
         <p class="text-lg font-normal text-gray-500 lg:text-xl text-gray-400">controle e versionamento de
             pedidos,cargas e clientes.</p>
@@ -60,46 +57,91 @@
         </div>
     </div>  --}}
 
-    
-<div id="accordion-collapse" class="pt-8" data-accordion="collapse">
-    <h2 id="accordion-collapse-heading-1">
-      <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 focus:ring-gray-800 border-gray-200 text-gray-400 hover:bg-gray-100  gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
-        <span>Cargas</span>
-        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-        </svg>
-      </button>
-    </h2>
-    <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-      <div class="p-5 border border-b-0 border-gray-200 border-gray-200 bg-gray-900">
-        <p class="mb-2 text-gray-500 text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
-        <p class="text-gray-500 text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" class="text-blue-600 text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
-      </div>
-    </div>
-    <h2 id="accordion-collapse-heading-2">
-      <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 focus:ring-gray-800 border-gray-200 text-gray-400 hover:bg-gray-100  gap-3" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
-        <span>Clientes</span>
-        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-        </svg>
-      </button>
-    </h2>
-    <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-      <div class="p-5 border border-b-0 border-gray-200 border-gray-200">
-        <p class="mb-2 text-gray-500 text-gray-400">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-        <p class="text-gray-500 text-gray-400">Check out the <a href="https://flowbite.com/figma/" class="text-blue-600 text-blue-500 hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
-      </div>
-    </div>
-    <h2 id="accordion-collapse-heading-3">
-      <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 focus:ring-gray-800 border-gray-200 text-gray-400 hover:bg-gray-100  gap-3" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
-        <span>Pedidos</span>
-        <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-        </svg>
-      </button>
-    </h2>
-  </div>
-  
-    
-</div>
 
+    <div id="accordion-collapse" class="pt-8" data-accordion="collapse">
+        <h2 id="accordion-collapse-heading-1">
+            <button type="button" wire:click="activecarga"
+                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 focus:ring-gray-800 border-gray-200 text-gray-400 hover:bg-gray-100  gap-3"
+                data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
+                aria-controls="accordion-collapse-body-1">
+                <span>Cargas</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5 5 1 1 5" />
+                </svg>
+            </button>
+        </h2>
+        @if ($flagcargas)
+            <div class="" >
+                <div class="p-5 border border-b-0 border-gray-200 border-gray-200 ">
+                    <p class="mb-2 text-gray-500 text-gray-400">Flowbite is an open-source library of interactive
+                        components
+                        built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                    <p class="text-gray-500 text-gray-400">Check out this guide to learn how to <a
+                            href="/docs/getting-started/introduction/"
+                            class="text-blue-600 text-blue-500 hover:underline">get started</a> and start developing
+                        websites even faster with components on top of Tailwind CSS.</p>
+                </div>
+            </div>
+        @endif
+
+
+        <h2 id="accordion-collapse-heading-1">
+            <button type="button" wire:click="activepedido"
+                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 focus:ring-gray-800 border-gray-200 text-gray-400 hover:bg-gray-100  gap-3"
+                data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
+                aria-controls="accordion-collapse-body-1">
+                <span>pedidos</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5 5 1 1 5" />
+                </svg>
+            </button>
+        </h2>
+        @if ($flagpedidos)
+            <div class="" >
+                <div class="p-5 border border-b-0 border-gray-200 border-gray-200 ">
+                    <p class="mb-2 text-gray-500 text-gray-400">Flowbite is an open-source library of interactive
+                        components
+                        built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                    <p class="text-gray-500 text-gray-400">Check out this guide to learn how to <a
+                            href="/docs/getting-started/introduction/"
+                            class="text-blue-600 text-blue-500 hover:underline">get started</a> and start developing
+                        websites even faster with components on top of Tailwind CSS.</p>
+                </div>
+            </div>
+        @endif
+
+
+        <h2 id="accordion-collapse-heading-1">
+            <button type="button" wire:click="activecliente"
+                class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 focus:ring-gray-800 border-gray-200 text-gray-400 hover:bg-gray-100  gap-3"
+                data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
+                aria-controls="accordion-collapse-body-1">
+                <span>clientes</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M9 5 5 1 1 5" />
+                </svg>
+            </button>
+        </h2>
+        @if ($flagclientes)
+            <div class="" >
+                <div class="p-5 border border-b-0 border-gray-200 border-gray-200 ">
+                    <p class="mb-2 text-gray-500 text-gray-400">Flowbite is an open-source library of interactive
+                        components
+                        built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
+                    <p class="text-gray-500 text-gray-400">Check out this guide to learn how to <a
+                            href="/docs/getting-started/introduction/"
+                            class="text-blue-600 text-blue-500 hover:underline">get started</a> and start developing
+                        websites even faster with components on top of Tailwind CSS.</p>
+                </div>
+            </div>
+        @endif
+    </div>
+
+
+</div>
